53839731292e39bf3c6cf2b652d5fdb0fa9cf031
Carlos <navarro.charlie.carlos@gmail.com>
2025-02-06 23:19:17 -0800

    add function buttons

--- callbacks.py
+++ callbacks.py
@@ -117,5 +117,78 @@ def register_callbacks(app):
     )
     def clear_changes(n_clicks):
         return []
+    
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-1-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_1(n_clicks, selected_rows):
+        print("Function 1")
+        print(selected_rows)
+        
+        return no_update
+
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-2-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_2(n_clicks, selected_rows):
+        print("Function 2")
+        print(selected_rows)
+        
+        return no_update
+
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-3-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_3(n_clicks, selected_rows):
+        print("Function 3")
+        print(selected_rows)
+        
+        return no_update
+    
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-4-1-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_4_1(n_clicks, selected_rows):
+        print("Function 4.1")
+        print(selected_rows)
+        
+        return no_update
+
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-4-2-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_4_2(n_clicks, selected_rows):
+        print("Function 4.2")
+        print(selected_rows)
+        
+        return no_update
+
+    @app.callback(
+        Output(component_id="dummy-div", component_property="children", allow_duplicate=True),
+        Input(component_id="function-4-3-button", component_property="n_clicks"),
+        State(component_id="database-table", component_property="selectedRows"),
+        prevent_initial_call=True
+    )
+    def function_4_3(n_clicks, selected_rows):
+        print("Function 4.3")
+        print(selected_rows)
+        
+        return no_update
+
 
     pass
--- layout.py
+++ layout.py
@@ -55,33 +55,35 @@ def serve_layout():
             dbc.Row([
                 html.H1("Manager")
             ]),
-            dbc.Row([
-                html.H2("Inputs")
-            ]),
-            dbc.Row([
-                dbc.Col([
-                    dbc.Row(html.Label("Input 1")),
-                    dbc.Row(dcc.Input(id="input-1", type="text", value="", placeholder="Enter input 1", debounce=True)),
-                    dbc.Row(html.Label("Input 2")),
-                    dbc.Row(dcc.Input(id="input-2", type="text", value="", placeholder="Enter input 2", debounce=True)),
+            dbc.Col([
+                dbc.Row([
+                    html.H2("Inputs")
                 ]),
-                dbc.Col([
-                    dbc.Row(html.Label("Input 3")),
-                    dbc.Row(dcc.Input(id="input-3", type="text", value="", placeholder="Enter input 3", debounce=True)),
-                    dbc.Row(html.Label("Input 4")),
-                    dbc.Row(dcc.Input(id="input-4", type="text", value="", placeholder="Enter input 4", debounce=True)),
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Row(html.Label("Input 1")),
+                        dbc.Row(dcc.Input(id="input-1", type="text", value="", placeholder="Enter input 1", debounce=True)),
+                        dbc.Row(html.Label("Input 2")),
+                        dbc.Row(dcc.Input(id="input-2", type="text", value="", placeholder="Enter input 2", debounce=True)),
+                    ]),
+                    dbc.Col([
+                        dbc.Row(html.Label("Input 3")),
+                        dbc.Row(dcc.Input(id="input-3", type="text", value="", placeholder="Enter input 3", debounce=True)),
+                        dbc.Row(html.Label("Input 4")),
+                        dbc.Row(dcc.Input(id="input-4", type="text", value="", placeholder="Enter input 4", debounce=True)),
+                    ]),
+                    dbc.Col([
+                        dbc.Row(html.Label("Input 5")),
+                        dbc.Row(dcc.Input(id="input-5", type="text", value="", placeholder="Enter input 5", debounce=True)),
+                        dbc.Row(html.Label("Input 6")),
+                        dbc.Row(dcc.Input(id="input-6", type="text", value="", placeholder="Enter input 6", debounce=True)),
+                    ]),
+                    dcc.Store(id="clear-inputs-flag", data=0, storage_type="session"),
                 ]),
-                dbc.Col([
-                    dbc.Row(html.Label("Input 5")),
-                    dbc.Row(dcc.Input(id="input-5", type="text", value="", placeholder="Enter input 5", debounce=True)),
-                    dbc.Row(html.Label("Input 6")),
-                    dbc.Row(dcc.Input(id="input-6", type="text", value="", placeholder="Enter input 6", debounce=True)),
+                dbc.Row([
+                    dbc.Button("Add to Database", id="add-to-db", color="primary", n_clicks=0),
+                    dbc.Button("Clear Inputs", id="clear-inputs-button", color="secondary", n_clicks=0),
                 ]),
-                dcc.Store(id="clear-inputs-flag", data=0, storage_type="session"),
-            ]),
-            dbc.Row([
-                dbc.Button("Add to Database", id="add-to-db", color="primary", n_clicks=0),
-                dbc.Button("Clear Inputs", id="clear-inputs-button", color="secondary", n_clicks=0),
             ]),
         ]),
         dbc.Container([
@@ -92,6 +94,20 @@ def serve_layout():
                 dag.AgGrid(id="database-table", rowData=None, columnDefs=None, dashGridOptions=serve_dash_grid_options()),
             ]),
             dcc.Store(id="database-store", data=[], storage_type="session"),
+            dbc.Row([
+                html.Div(id="dummy-div", style={"display":"none"}), # Dummy div to create callbacks with no outputs
+                dbc.Button("Function 1", id="function-1-button", color="primary", n_clicks=0),
+                dbc.Button("Function 2", id="function-2-button", color="primary", n_clicks=0),
+                dbc.Button("Function 3", id="function-3-button", color="primary", n_clicks=0),
+                dbc.DropdownMenu(
+                    label="Function 4",
+                    children=[
+                        dbc.DropdownMenuItem("Function 4.1", id="function-4-1-button", n_clicks=0),
+                        dbc.DropdownMenuItem("Function 4.2", id="function-4-2-button", n_clicks=0),
+                        dbc.DropdownMenuItem("Function 4.3", id="function-4-3-button", n_clicks=0),
+                    ],
+                ),
+            ])
         ]),
         dbc.Container([
             dbc.Row([
