545a268dcbffd3853b0f83d272089b7c47d32270
Carlos <navarro.charlie.carlos@gmail.com>
2025-02-06 19:36:01 -0800

    add some db row-column helper functions

--- database.py
+++ database.py
@@ -11,6 +11,7 @@ taperunner_columns = [
     "Input4",
     "Input5",
     "Input6",
+    "Status",
 ]
 
 
@@ -51,9 +52,10 @@ def initialize_database(db_file=None, table_name=None, columns=None):
     )
     conn.commit()
     conn.close()
-    print(
-        f"Database '{db_file}' initialized with table '{table_name}' and columns '{columns}'."
-    )
+    
+    for column in columns[1:]:
+        if column not in get_columns(db_file, table_name):
+            add_column(column, db_file, table_name)
 
 
 def add_row(row_data, db_file=None, table_name=None):
@@ -70,6 +72,66 @@ def add_row(row_data, db_file=None, table_name=None):
     placeholders = ", ".join(["?"] * len(row_data))
     query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
     cursor.execute(query, tuple(row_data.values()))
+    conn.commit()
+    conn.close()
+    
+    
+def delete_row(row_id, db_file=None, table_name=None):
+    """Deletes a single row from the specified table."""
+    if db_file is None:
+        db_file = taperunner_db
+
+    if table_name is None:
+        table_name = taperunner_table_name
+        
+    if row_id is None:
+        return
+    
+    if row_id not in get_table_as_df(db_file, table_name)["id"].values:
+        return
+
+    conn = get_db_connection(db_file)
+    cursor = conn.cursor()
+    query = f"DELETE FROM {table_name} WHERE id = ?"
+    cursor.execute(query, (row_id,))
+    conn.commit()
+    conn.close()
+
+
+def add_column(column_name, db_file=None, table_name=None):
+    """Adds a single column to the specified table."""
+    if db_file is None:
+        db_file = taperunner_db
+
+    if table_name is None:
+        table_name = taperunner_table_name
+        
+    if column_name in get_columns(db_file, table_name):
+        return
+
+    conn = get_db_connection(db_file)
+    cursor = conn.cursor()
+    query = f"ALTER TABLE {table_name} ADD COLUMN {column_name}"
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
+    
+    
+def delete_column(column_name, db_file=None, table_name=None):
+    """Deletes a single column from the specified table."""
+    if db_file is None:
+        db_file = taperunner_db
+
+    if table_name is None:
+        table_name = taperunner_table_name
+        
+    if column_name not in get_columns(db_file, table_name):
+        return
+
+    conn = get_db_connection(db_file)
+    cursor = conn.cursor()
+    query = f"ALTER TABLE {table_name} DROP COLUMN {column_name}"
+    cursor.execute(query)
     conn.commit()
     conn.close()
 
